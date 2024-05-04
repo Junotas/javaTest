9fe54a3fab5890d9b3c100c5d29d14e9e2d7989f
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-05-04 12:56:55 +0200

    Java test for 2+2 == 4

---
+++ src/test/java/SimpleCalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleCalculatorTest {
+
+
+    @Test
+
+    void twoPLusTwoShouldEqualFour() {
+
+        var calculator = new SimpleCalculator();
+        assertEquals(4, calculator.add(2, 2));
+
+
+    }
+}
