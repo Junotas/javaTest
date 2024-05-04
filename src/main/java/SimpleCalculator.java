650a564b5a826787e5eac7ed0cc8df8a51193150
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-05-04 13:06:50 +0200

    Added different operators

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -3,5 +3,8 @@ public class SimpleCalculator {
     public int add (int numberA, int numberB) {
 
         return numberA * numberB;
+        //return numberA - number B;
+        //return numberA + number B;
+        //return numberA / number B;
     }
 }
